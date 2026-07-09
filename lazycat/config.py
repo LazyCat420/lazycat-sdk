4e5eb490a269b94bbe57dec428b9ce06040e8743
LazyCat <85379465+LazyCat420@users.noreply.github.com>
2026-07-08 17:14:19 -0700

    Fix PRISM_URL fallback bug

--- lazycat/config.py
+++ lazycat/config.py
@@ -9,11 +9,11 @@ def get_env_or_raise(key: str) -> str:
 class Config:
     @property
     def PRISM_URL(self):
-        return get_env_or_raise("PRISM_URL")
+        return os.getenv("PRISM_URL", "http://10.0.0.16:7777")
 
     @property
     def VAULT_SERVICE_URL(self):
-        return get_env_or_raise("VAULT_SERVICE_URL")
+        return os.getenv("VAULT_SERVICE_URL", "http://10.0.0.16:5599")
 
     @property
     def LAZY_TOOL_SERVICE_PORT(self):
